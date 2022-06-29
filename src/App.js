45273b251a6d1125de3006cbfd1259d3f9bbebbc
kamal <rajput.kamal40@gmail.com>
2022-06-29 20:36:38 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import UserDetail from './components/UserDetail';
 import UserList from './components/UserList';
 
 function App() {
   return (
     <div className="container mx-auto">
       <UserList />
+      <UserDetail />
     </div>
   );
 }
--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React,{useState, useEffect } from "react";
 import { MdArrowBackIosNew } from "react-icons/md";
 
 
 
 const UserDetail = () => {
-  // const url = "https://dummyapi.io/data/v1/user/60d0fe4f5311236168a109ca";
 
-  // fetch(url, {
-  //   method: "GET",
-  //   headers: new Headers({
-  //     "app-id": "60b1a6fbbf43a7e4bef68947",
-  //   }),
-  // }).then(async function (response) {
-  //   const data = await response.json();
-  //   console.log(data);
-  // });
 
+const url = "https://dummyapi.io/data/v1/user/60d0fe4f5311236168a109ca";
+  
+  useEffect(()=>{
+    const getSingleUser = async () => {
+      try {
+        const res = await fetch(url, {
+          method: "GET",
+          headers: new Headers({
+            "app-id": "60b1a6fbbf43a7e4bef68947",
+          })})
+        const data = await res.json()
+        console.log(data)
+      } catch(err) {
+        console.log("err: ", err)
+      }
+    }
+  
+    getSingleUser()
+  
+  },[])
+  
   return (
     <>
       <div className="flex justify-around border-2 border-red-600 p-6 bg-white rounded-md shadow-lg mt-4 gap-6">
